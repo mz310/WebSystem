cb004b382e59714b4cd22034f7e0e999d7a169cc
mz310 <135813132+mz310@users.noreply.github.com>
2024-11-21 14:58:21 +0800

    Add files via upload

---
+++ lab5/AuthContext.js
@@ -0,0 +1,26 @@
+// src/AuthContext.js
+
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState(null);
+
+  const login = (id) => {
+    setIsAuthenticated(true);
+    setUserId(id);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUserId(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, userId, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
